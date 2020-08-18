2426b244e77d97a8423b47e854d185afd23764eb
SoutaMatsukawa <65427752+SoutaMatsukawa@users.noreply.github.com>
2020-08-19 03:50:47 +0900

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(input.lightLevel())
+})
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+})
+basic.forever(function () {
+	
+})
